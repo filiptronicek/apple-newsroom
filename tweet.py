ec25efb90dad2dccdb3817fbfbab735aa411752c
Filip Troníček <filip.tronicek@seznam.cz>
2020-06-06 18:35:36 +0200

    Update tweet.py

--- tweet.py
+++ tweet.py
@@ -3,7 +3,7 @@ from os import environ as env
 
 def tweet(tweetText:str):
     if env.get('CONSUMER_KEY') is None:
-        from creds import API_key, CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET
+        from creds import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET
     else:
         CONSUMER_KEY = env.get('CONSUMER_KEY')
         CONSUMER_SECRET = env.get('CONSUMER_SECRET')
