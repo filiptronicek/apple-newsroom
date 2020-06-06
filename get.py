53887177a06765c0058418116e0f8d2c99f80177
Filip Troníček <filip.tronicek@seznam.cz>
2020-06-06 22:23:58 +0200

    Update get.py

--- get.py
+++ get.py
@@ -1,10 +1,23 @@
 import feedparser
 from shorten import shorten
+
+from tweet import tweet
+
 NewsFeed = feedparser.parse("https://www.apple.com/newsroom/rss-feed.rss")
 
-entry = NewsFeed.entries[1]
-
 exShrt = "chl.li/xXxXx"
+
+def cut(content, limit = 280):
+    moreTxt = "..."
+    limit = limit - len(moreTxt) - len(exShrt)
+    if len(content) >= limit:
+        content = content[:limit]+moreTxt
+    return content
+
+def prprTxt(entry):
+    tweetText = cut(entry.summary.replace("<br/>", ". ").replace("<br />", ". ").split("Press Contacts")[0])
+    tweetText += "\n"+shorten(entry.link)
+    return tweetText
 
 def check():
     with open('used.txt') as f:
@@ -19,19 +32,9 @@ def check():
             pass
         else:
             print("New article!")
+            tweet(prprTxt(entryN))
             with open("used.txt", "a") as f:
                 f.writelines(id+"\n")
         
 
-def cut(content, limit = 280):
-    moreTxt = "..."
-    limit = limit - len(moreTxt) - len(exShrt)
-    if len(content) >= limit:
-        content = content[:limit]+moreTxt
-    return content
-
-def prprTxt():
-    tweetText = cut(entry.summary.replace("<br/>", ". ").replace("<br />", ". ").split("Press Contacts")[0])
-    tweetText += "\n"+shorten(entry.link)
-    return tweetText
 check()
