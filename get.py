07cc93d466b5c81c5a5464e0dba095f9e7cc3f1b
Filip Troníček <filip.tronicek@seznam.cz>
2020-06-06 20:30:23 +0200

    Check for articles

--- get.py
+++ get.py
@@ -6,6 +6,23 @@ entry = NewsFeed.entries[1]
 
 exShrt = "chl.li/xXxXx"
 
+def check():
+    with open('used.txt') as f:
+        used=f.readlines()
+    for i,entryN in enumerate(NewsFeed.entries[:5]):
+        id = entryN.id.split("/")[-1]
+        usedIds = []
+        for ind in used:
+            usedIds.append(ind.split("\n")[0])
+
+        if id in usedIds:
+            pass
+        else:
+            print("New article!")
+            with open("used.txt", "a") as f:
+                f.writelines(id+"\n")
+        
+
 def cut(content, limit = 280):
     moreTxt = "..."
     limit = limit - len(moreTxt) - len(exShrt)
@@ -17,3 +34,4 @@ def prprTxt():
     tweetText = cut(entry.summary.replace("<br/>", ". ").replace("<br />", ". ").split("Press Contacts")[0])
     tweetText += "\n"+shorten(entry.link)
     return tweetText
+check()
